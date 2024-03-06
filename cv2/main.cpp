bd7e31405022fe642cf7b8a100747e4934da657e
tylmarek1 <tylmarek@outlook.com>
2024-03-06 09:52:26 +0100

    cv2 done

--- cv2/main.cpp
+++ cv2/main.cpp
@@ -22,29 +22,88 @@ void ukol1() {
 	return;
 }
 
-void nextlevel(int curlevel, Tree1 *curtree, int maxlevel) {
-	if (curlevel = maxlevel) return;
-	curlevel++;
-	nextlevel(curlevel, curtree->CreateNext1(curlevel, "ahoj"), maxlevel);
-	nextlevel(curlevel, curtree->CreateNext2(curlevel, "ahoj"), maxlevel);
-}
+
 
 void ukol2() {
-	Tree1* tr1 = new Tree1(0, "ahoj");
-	int level = 0;
-	nextlevel(level, tr1, 5);
-	Tree1* tr = tr1;
-	Tree1* trh;
-	for (int i = 0; i < 5; i++)
-	{
-		std::cout << tr->GetValue() << std::endl;
-		trh = tr->GetNext1();
-		tr = trh;
+	Tree1* tr1 = new Tree1(0, "Je to zvire?");
+	Tree1* tr11 = tr1->CreateNext1(11, "Je to savec?");
+	Tree1* tr111 = tr11->CreateNext1(111, "Je to prase?");
+	Tree1* tr112 = tr11->CreateNext2(112, "Je to had?");
+
+	Tree1* tr12 = tr1->CreateNext2(12, "Je to kamen?");
+	Tree1* tr121 = tr12->CreateNext1(121, "Je to strom?");
+	Tree1* tr122 = tr12->CreateNext2(122, "Je to zula?");
+
+	char answer;
+	std::cout << tr1->GetValue() << std::endl;
+	std::cout << "(Y/N): ";
+	std::cin >> answer;
+	std::cout << std::endl;
+	if (answer == 'Y') {
+		std::cout << tr11->GetValue() << std::endl;
+		std::cout << "(Y/N): ";
+		std::cin >> answer;
+		std::cout << std::endl;
+		if (answer == 'Y') {
+			std::cout << tr111->GetValue() << std::endl;
+			std::cout << "(Y/N): ";
+			std::cin >> answer;
+			std::cout << std::endl;
+			if (answer == 'Y') {
+				std::cout << "je to prase..." << std::endl;
+			}
+			else {
+				std::cout << "je to pes..." << std::endl;
+			}
+		}
+		else {
+			std::cout << "je to had..." << std::endl;
+		}
+	}
+	else {
+		std::cout << tr12->GetValue() << std::endl;
+		std::cout << "(Y/N): ";
+		std::cin >> answer;
+		std::cout << std::endl;
+		if (answer == 'Y') {
+			std::cout << "je to kamen..." << std::endl;
+		}
+		else {
+			std::cout << tr121->GetValue() << std::endl;
+			std::cout << "(Y/N): ";
+			std::cin >> answer;
+			std::cout << std::endl;
+			if (answer == 'Y') {
+				std::cout << "je to strom..." << std::endl;
+			}
+			else {
+				std::cout << "je to kytka..." << std::endl;
+			}
+		}
 	}
 }
 
+void printouttree(Tree1* tr, int max, int cur) {
+	if (cur > max) return;
+	for (int i = 0; i < cur; i++) std::cout << "....";
+	std::cout << tr->GetKey() << " - " << tr->GetValue() << std::endl;
+	printouttree(tr->GetNext1(), max, cur + 1);
+	printouttree(tr->GetNext2(), max, cur + 1);
+}
+
+
 int main() {
-	ukol1();
-	ukol2();
+	//ukol1();
+	//ukol2();
+	Tree1* tr1 = new Tree1(0, "Je to zvire?");
+	Tree1* tr11 = tr1->CreateNext1(11, "Je to savec?");
+	Tree1* tr111 = tr11->CreateNext1(111, "Je to prase?");
+	Tree1* tr112 = tr11->CreateNext2(112, "Je to had?");
+
+	Tree1* tr12 = tr1->CreateNext2(12, "Je to kamen?");
+	Tree1* tr121 = tr12->CreateNext1(121, "Je to strom?");
+	Tree1* tr122 = tr12->CreateNext2(122, "Je to zula?");
+
+	printouttree(tr1, 2, 0);
 	return 0;
 }
